026db46db00f7e30b52480add8ee6d7901ba1c1d
Malisa93 <onyekafelix15@gmail.com>
2023-04-20 23:29:37 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-        printf (“Size of a char. %  byte(s) n” , sizeof (char);
+        printf(“Size of a char. %  byte(s) n” , sizeof (char);
 printf (“Size of a int. %  byte(s) n” , sizeof (int);  
 printf (“Size of a long int. %  byte(s) n” , sizeof (long int);
 printf (“Size of a long long int. %  byte(s) n” , sizeof (long long int);
 printf (“Size of a float. %  byte(s) n” , sizeof (float);
-  return (0);
+
+    return (0);
 }

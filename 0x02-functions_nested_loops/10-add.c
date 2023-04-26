bd7898137602173f5c05d42d188228fb8806143e
Malisa93 <onyekafelix15@gmail.com>
2023-04-26 11:07:08 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add-print the result of an integers
+ * @a:first num to add
+ * @b:secound num to add
+ * Return:n
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
